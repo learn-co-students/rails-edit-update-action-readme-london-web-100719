e576260deb4ffd146c388950135c786ad342969f
Ned-el-ch <nedelchev.niki@gmail.com>
2019-11-04 16:04:47 +0000

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,49 @@
 class ArticlesController < ApplicationController
-  def index
-    @articles = Article.all
-  end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+    before_action :set_article, only: [:show, :edit, :update]
 
-  def new
-    @article = Article.new
-  end
+    def index
+        @articles = Article.all
+    end
 
-  def create
-    @article = Article.new
-    @article.title = params[:title]
-    @article.description = params[:description]
-    @article.save
-    redirect_to article_path(@article)
-  end
+    def show
+        set_article
+    end
 
-  # add edit and update methods here
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new
+        @article.title = params[:title]
+        @article.description = params[:description]
+        @article.save
+        redirect_to article_path(@article)
+    end
+
+    def edit
+        set_article
+    end
+
+    def update
+        # byebug
+        set_article.update(article_params)
+        redirect_to article_path(@article)
+    end
+
+private
+
+    def set_article
+
+        @article = Article.find(params[:id])
+        
+    end
+
+    def article_params
+
+        params.require(:article).permit(:title, :description)
+        
+    end
+    # add edit and update methods here
 end
