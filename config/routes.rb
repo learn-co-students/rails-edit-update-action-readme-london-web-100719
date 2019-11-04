f6f6a65308ef5ef129b10c1b379d05bffcd976a9
flow1981 <florian.stagliano@gmx.de>
2019-11-04 17:09:34 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#show
+get 'articles/:id', to: 'articles#show'
+
+
+# #edit
+# get 'articles/:id/edit', to: 'articles#edit', as: :edit_articles
+
+#update
+#patch 'articles/:id', to: 'articles#update'
+
+#resources :articles, only: [:index, :new, :show, :edit, :create, :update]
+resources :articles, only: [:index, :new, :create, :edit, :update]
 end
